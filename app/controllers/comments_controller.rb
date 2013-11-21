9cac3588f4be523936ddee4f9e34a535b0c2caee
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-21 07:41:58 -0800

    removed offending curly brace

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,6 +31,6 @@ class CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to comments_url }
+    redirect_to comments_url
   end
 end
